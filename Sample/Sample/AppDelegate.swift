ba4c7bedeebd0f859635f0b73037edec7143a780
Barbara Rollet <barbara.rollet@yoti.com>
2019-08-30 12:34:35 +0100

    DEP-87_1.0.0 Initial release

---
+++ Sample/Sample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  Yoti Doc Scan iOS SDK, Sample.
+//  Created by Barbara Rollet.
+//
+//  Copyright (c) Yoti Ltd 2019.
+//  All rights reserved.
+//  See LICENSE.md for license.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
